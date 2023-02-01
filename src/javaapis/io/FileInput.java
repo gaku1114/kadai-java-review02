8c4ece5888f82566156d8c8290f710545917a188
gaku1114 <ga1114ku@gmail.com>
2023-02-02 02:26:10 +0900

    lesson6

---
+++ src/javaapis/io/FileInput.java
@@ -0,0 +1,32 @@
+package javaapis.io;
+
+public class FileInput {
+
+    public static void main(String[] args) {
+        try {
+            // ファイルを読み込みモードで開く
+            InputStream is = new FileInputStream("test.txt");
+
+            // ファイルから読み込む操作をするオブジェクトを生成する
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+            // 1行分だけ取得
+            String str = reader.readLine();
+
+            // strが空っぽでなければ中身を表示して
+            // さらに次の行の内容を取得
+            while(str != null) {
+                System.out.println(str);
+                str = reader.readLine();
+            }
+            // ファイルを閉じる(readerとisの使用をやめる）
+            reader.close();
+            is.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
